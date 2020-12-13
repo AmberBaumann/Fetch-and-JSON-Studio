2e8e6affd0471a4967ee915933081f3b23c1c911
Amber Baumann <ambermindsolliving@gmail.com>
2020-12-13 15:28:21 -0600

    updated

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const astronauts = document.getElementById('astronauts');
-const count = document.getElementById('count');
-const loadAstronauts = async () => {
-    try {
-        const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
-        const data = await response.json();
-        console.log(data);
-        count.innerText = `${data.length} Astronauts`;
-        const htmlString = data
-            .sort((a, b) => a.hoursInSpace - b.hoursInSpace)
-            .map(
-                (astronaut) => `
-                    <div class = "astronaut">
-                        <h3>${astronaut.firstName} - ${astronaut.lastName}</h3>
-                    </div>
-                    `
-            )
-            .join(' ');
-    } catch (err) {
-        console.error(err);
-    }
-};
+// const astronauts = document.getElementById('astronauts');
+// const count = document.getElementById('count');
+// const loadAstronauts = async () => {
+//     try {
+//         const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// };
