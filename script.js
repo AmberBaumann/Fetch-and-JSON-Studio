6c9f74aefd1abc1108188090dd0ea904b170960d
Amber Baumann <ambermindsolliving@gmail.com>
2020-12-13 16:56:31 -0600

    rework

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// const astronauts = document.getElementById('astronauts');
-// const count = document.getElementById('count');
-// const loadAstronauts = async () => {
-//     try {
-//         const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
-//         const data = await response.json();
-//         console.log(data);
-//     } catch (err) {
-//         console.error(err);
-//     }
-// };
+window.addEventListener('load', function () {
+    const url = "https://handlers.education.launchcode.org/static/astronauts.json";
+    fetch(url).then(function (response) {
+        return response.json;
+    }).then(function (json)) {
+
+        const container = document.querySelector('#container');
+        let astronauts = '';
+    }
+})
